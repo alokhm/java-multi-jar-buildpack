03900de8d6fe31d432416d6f7c2add28dd252e32
alokhm <akshukla226@gmail.com>
2015-02-12 18:38:47 +0530

    Update tomcat_access_logging_support.rb

--- lib/java_buildpack/container/tomcat/tomcat_access_logging_support.rb
+++ lib/java_buildpack/container/tomcat/tomcat_access_logging_support.rb
@@ -34,7 +34,7 @@ module JavaBuildpack
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
         @droplet.java_opts.add_system_property 'access.logging.enabled', @configuration[KEY_ENABLED] == 'enabled'
-        #@droplet.java_opts.add_system_property 'access.logging1.enabled', @configuration[KEY_ENABLED] == 'enabled'
+        @droplet.java_opts.add_system_property 'access.logging1.enabled', @configuration[KEY_ENABLED] == 'enabled'
       end
 
       protected
@@ -47,7 +47,7 @@ module JavaBuildpack
       private
 
       KEY_ENABLED = 'access_logging'.freeze
-
+      KEY_ENABLED = 'access_logging1'.freeze
       def jar_name
         "tomcat_access_logging_support-#{@version}.jar"
         
