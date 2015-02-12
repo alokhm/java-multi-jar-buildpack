17cdf19c1698ae0e0d50dc96db1d37aa44119de0
alokhm <akshukla226@gmail.com>
2015-02-12 17:42:33 +0530

    Update tomcat.rb

--- lib/java_buildpack/container/tomcat.rb
+++ lib/java_buildpack/container/tomcat.rb
@@ -51,6 +51,7 @@ module JavaBuildpack
           TomcatLifecycleSupport.new(sub_configuration_context(context, 'lifecycle_support')),
           TomcatLoggingSupport.new(sub_configuration_context(context, 'logging_support')),
           TomcatAccessLoggingSupport.new(sub_configuration_context(context, 'access_logging_support')),
+          TomcatAccessLoggingSupport.new(sub_configuration_context(context, 'access_logging_support1')),
           TomcatRedisStore.new(sub_configuration_context(context, 'redis_store')),
           TomcatGemfireStore.new(sub_configuration_context(context, 'gemfire_store')),
           TomcatInsightSupport.new(context)
